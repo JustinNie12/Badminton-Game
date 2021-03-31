fae629f60930e93fd4ec4f277f81da400b676e1d
JustinNie12 <56981336+JustinNie12@users.noreply.github.com>
2021-03-30 21:04:01 -0500

    Added flip to update function

--- Mobile/Assets/Scripts/MovementJoystick.cs
+++ Mobile/Assets/Scripts/MovementJoystick.cs
@@ -18,6 +18,19 @@ public class MovementJoystick : MonoBehaviour
         joystickOriginalPos = joystickBG.transform.position;
         joystickRadius = joystickBG.GetComponent<RectTransform>().sizeDelta.y / 4;
     }
+    void Update()
+    {
+        Vector3 characterScale = transform.localScale;
+        if(Input.GetAxis("Horizontal") < 0)
+        {
+            characterScale.x = -10;
+        }
+        if (Input.GetAxis("Horizontal") < 0)
+        {
+            characterScale.x = 10;
+        }
+        transform.localScale = characterScale;
+    }
 
     public void PointerDown()
     {
